fde72e349585450fc0ddea0288e5ada9cf9a8167
Pungraf <johny793@gmail.com>
2019-05-18 00:30:12 +0200

    Player Controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    private PlayerController controller;
+    private Camera camera;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<PlayerController>();
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            controller.LookAt(point);
+        }
         
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    private Vector3 velocity;
+    private Rigidbody thisRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void FixedUpdate()
+    {
+        thisRb.MovePosition(thisRb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightOffsetPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightOffsetPoint);
     }
 }
