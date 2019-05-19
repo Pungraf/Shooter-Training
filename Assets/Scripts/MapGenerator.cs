af587b4b886f32a4e062736bf128387b9bb1d826
Pungraf <johny793@gmail.com>
2019-05-19 22:05:29 +0200

    Map generator

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+
+    [Range(0, 1)]
+    public float outlinePrecent;
+    
+    void Start()
+    {
+        GenerateMap();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void GenerateMap()
+    {
+        string holderName = "Generated map";
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+        
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+        
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x/2 + .5f + x, 0f, -mapSize.y/2 + .5f + y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90));
+                newTile.localScale = Vector3.one * (1 - outlinePrecent);
+                newTile.parent = mapHolder;
+            }
+        }
+    }
+}
