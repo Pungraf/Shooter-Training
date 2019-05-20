d2ac4743e9a3b315b169183f696450ff6e42cbe9
Pungraf <johny793@gmail.com>
2019-05-20 18:29:25 +0200

    Map obstacles random placement

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;
+    public Transform obstaclePrefab;
     public Vector2 mapSize;
 
     [Range(0, 1)]
     public float outlinePrecent;
+
+    public int seed = 10;
+
+    private List<Coord> allTileCords;
+    private Queue<Coord> shuffledTIleCoords;
     
     void Start()
     {
@@ -22,6 +28,16 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
+        allTileCords = new List<Coord>();
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                allTileCords.Add(new Coord(x, y));
+            }
+        }
+        shuffledTIleCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCords.ToArray(), seed));
+
         string holderName = "Generated map";
         if (transform.Find(holderName))
         {
@@ -35,11 +51,44 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3(-mapSize.x/2 + .5f + x, 0f, -mapSize.y/2 + .5f + y);
+                Vector3 tilePosition = CordToPosition(x, y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90));
                 newTile.localScale = Vector3.one * (1 - outlinePrecent);
                 newTile.parent = mapHolder;
             }
+        }
+
+        int obstacleCount = 10;
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coord randomCoord = GetRandomCoord();
+            Vector3 obstaclePosition = CordToPosition(randomCoord.x, randomCoord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * .5f, Quaternion.identity);
+            newObstacle.parent = mapHolder;
+        }
+    }
+
+    Vector3 CordToPosition(int x, int y)
+    {
+        return  new Vector3(-mapSize.x/2 + .5f + x, 0f, -mapSize.y/2 + .5f + y);
+    }
+
+    public Coord GetRandomCoord()
+    {
+        Coord randomCord = shuffledTIleCoords.Dequeue();
+        shuffledTIleCoords.Enqueue(randomCord);
+        return randomCord;
+    }
+    
+    public struct Coord
+    {
+        public int x;
+        public int y;
+
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
         }
     }
 }
